84b5babbbdcd580cae2834e4814e6244be2e0588
Zpatato <1443090176@qq.com>
2021-11-19 15:27:23 +0800

    添加红黑榜超链接，修改登录功能

--- AIUI/main/js/login.js
+++ AIUI/main/js/login.js
@@ -17,17 +17,22 @@ function signInButton(){
 
 function signIn(username, password){
     $.ajax({
-        url: baseUrl + 'api/login',
+        url: baseUrl + 'api/user/login',
         contentType: 'application/json',
         type: 'post',
         data: JSON.stringify({
             username: username,
             password: password
         }),
-        success: function (data){
+        success: function (ret){
+            ret = ret.replaceAll('ObjectId(', '')
+            ret = ret.replaceAll(')', '')
+            ret = ret.replaceAll('\'', '"')
+            data=JSON.parse(ret);
+            $.cookie('userid', data._id, { expires: 7, path: '/' })
             $.cookie('username', username, { expires: 7, path: '/' })
             $.cookie('password', password, { expires: 7, path: '/' })
-            console.log($.cookie('username'), $.cookie('password'))
+            console.log($.cookie('userid'),$.cookie('username'), $.cookie('password'),)
             window.location.href = "items.html";
         },
         error: function (err){
