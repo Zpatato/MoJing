39b20f9ac1372ebeecb7e46128788724faa9d2a0
Zpatato <1443090176@qq.com>
2021-11-19 20:25:59 +0800

    修改

--- AIUI/main/js/items.js
+++ AIUI/main/js/items.js
@@ -179,7 +179,7 @@ function submitScore() {
         }
         $.ajax(
             {
-                url: baseUrl + 'api/scoring',
+                url: baseUrl + 'api/update/score',
                 contentType: 'application/json',
                 type: 'post',
                 data: JSON.stringify(r),
